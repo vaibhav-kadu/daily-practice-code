cf464c5eb841ff8c7a68e6e4688a882f17d65581
Vaibhav Kadu <vkdon02@gmail.com>
2024-12-17 00:00:00 +0530

    update: spring/demo/src/main/java/com/example/demo/DemoApplication.java

---
+++ spring/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+# commit on 2024-12-17 00:00:00
