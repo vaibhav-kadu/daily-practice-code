3dd5470cdd1bf1d9841c67327f34df89d5bc4bca
Vaibhav Kadu <vkdon02@gmail.com>
2025-02-21 00:00:00 +0530

    update: spring/JDBC/src/test/java/com/example/jdbc/JdbcApplicationTests.java

---
+++ spring/JDBC/src/test/java/com/example/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+# commit on 2025-02-21 00:00:00
