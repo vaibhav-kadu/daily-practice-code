5ee9cf851715df5b1ac3c9d2e1fe6b59ea3ec5f8
Vaibhav Kadu <vkdon02@gmail.com>
2025-04-15 00:00:00 +0530

    update: spring/demo/src/main/java/com/example/demo/ServletInitializer.java

---
+++ spring/demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
+
+# commit on 2025-04-15 00:00:00
