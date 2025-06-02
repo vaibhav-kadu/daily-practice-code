211ad796890ac4ee9d771774d4a0afb2bdf6c954
Vaibhav Kadu <vkdon02@gmail.com>
2025-06-03 00:00:00 +0530

    update: React/React JS/01-folder/src/App.jsx

---
+++ React/React JS/01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <h3>Hello</h3>
+    </div>
+  )
+}
+
+export default App
+# commit on 2025-06-03 00:00:00
