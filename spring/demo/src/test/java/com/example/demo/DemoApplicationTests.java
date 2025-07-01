5b16530211f73b0f80e47feb59a382f4bc621778
Vaibhav Kadu <vkdon02@gmail.com>
2025-07-02 00:00:00 +0530

    update: spring/demo/src/test/java/com/example/demo/DemoApplicationTests.java

---
+++ spring/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+# commit on 2025-07-02 00:00:00
