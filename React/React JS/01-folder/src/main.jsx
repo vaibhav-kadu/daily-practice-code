c44c52568e84bee2c85692b70d50ceeff915f4e5
Vaibhav Kadu <vkdon02@gmail.com>
2025-08-07 00:00:00 +0530

    update: React/React JS/01-folder/src/main.jsx

---
+++ React/React JS/01-folder/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+ <App />
+)
+
+# commit on 2025-08-07 00:00:00
