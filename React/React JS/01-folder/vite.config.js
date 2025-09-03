9f70fd6a7d74ef16cd73badbb0bb96d92a39af47
Vaibhav Kadu <vkdon02@gmail.com>
2025-09-04 00:00:00 +0530

    update: React/React JS/01-folder/vite.config.js

---
+++ React/React JS/01-folder/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+# commit on 2025-09-04 00:00:00
